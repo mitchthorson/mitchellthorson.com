f5b636a6e32b2987d7f2ef578524f60aacc89ea6
mitchthorson <mitchell@mitchellthorson.com>
2023-11-12 02:26:04 -0500

    WIP design iteration

--- src/lib/components/ProjectList/ProjectItem.svelte
+++ src/lib/components/ProjectList/ProjectItem.svelte
@@ -9,6 +9,7 @@
    * @property {string} organization
    */
 </script>
+
 <script>
   import { timeFormat } from "d3-time-format";
   /** @type {Project} */
@@ -16,7 +17,7 @@
 
   export let show_images = true;
 
-	const formatTime = timeFormat("%B %Y");
+  const formatTime = timeFormat("%B %Y");
 </script>
 
 <li class="project-item">
@@ -24,16 +25,27 @@
     <img src={project.image} alt={project.alt_text} class="project-list-item-thumbnail" />
   {/if}
   <div class="project-item-text-wrap">
-		<h3 class="project-item-title"><a href={project.url} class="project-list-item-link" target="_blank">{project.title}</a></h3>
-    <p class="project-item-date">{formatTime(project.date)}  {project.organization}</p>
+    <h3 class="project-item-title">
+      <a href={project.url} class="project-list-item-link" target="_blank">{project.title}</a>
+    </h3>
+    <p class="project-item-date">
+      {formatTime(project.date)} <span class="project-org">{project.organization}</span>
+    </p>
   </div>
 </li>
 
 <style>
-.project-item {
-	margin: 3rem 0;
-}
-.project-item-thumbnail {
-	width: 180px;
-}
+  .project-item {
+    margin: 3rem 0;
+  }
+  .project-item-thumbnail {
+    width: 180px;
+  }
+  .project-item-title {
+    font-weight: bold;
+		margin-bottom: 0.25em;
+  }
+  .project-org {
+    font-style: italic;
+  }
 </style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<div class="content-wrap">
+<slot></slot>
+</div>
+<style>
+.content-wrap {
+	padding: 20px;
+	margin: 0 auto;
+	max-width: 900px;
+}
+</style>
