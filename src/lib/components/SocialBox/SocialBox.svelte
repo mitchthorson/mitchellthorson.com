2b329a73dcd90c16af6a0a0312c81aabca6296ef
mitchthorson <mitchell@mitchellthorson.com>
2023-11-12 01:48:42 -0500

    WIP design iteration

---
+++ src/lib/components/SocialBox/SocialBox.svelte
@@ -0,0 +1,38 @@
+<script>
+  /** @typedef {Object} SocialLink
+   * @property {string} platform
+   * @property {string} url
+   */
+  /** @type {SocialLink[]} */
+  export let social_links;
+</script>
+
+<section class="social-link-box">
+  <div class="inner-wrap">
+    <p>Find me around the internet:</p>
+    <ul>
+      {#each social_links as link}
+        <li>
+          <a href={link.url} class="social-link" target="_blank">{link.platform}</a>
+        </li>
+      {/each}
+    </ul>
+  </div>
+</section>
+
+<style>
+  .social-link-box {
+    margin: 3rem 0;
+    /* max-width: 200px; */
+    /*border: solid 1px var(--color-accent); */
+    /* padding: 1rem;*/
+  }
+  li {
+    margin: 0.5rem 0;
+  }
+  li::before {
+    display: inline-block;
+    content: "-";
+    width: 1em;
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,7 @@ const config = {
     alias: {
       "$components/*": "./src/lib/components/*",
       "$data/*": "./src/data/*",
+			"$styles/*": "./src/lib/styles/*",
     },
   },
 };
