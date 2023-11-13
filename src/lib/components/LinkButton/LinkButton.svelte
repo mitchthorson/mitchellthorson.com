be7fedf8f77813d5f919c5cdc3acd70a0f3e3f86
mitchthorson <mitchell@mitchellthorson.com>
2023-11-12 22:07:08 -0500

    WIP design updates

---
+++ src/lib/components/LinkButton/LinkButton.svelte
@@ -0,0 +1,28 @@
+<script>
+	/** @type {string} */
+  export let href;
+</script>
+
+<button class="link-button">
+  <a {href} >
+    <slot />
+  </a>
+</button>
+
+<style>
+.link-button {
+	appearance: none;
+	background: none;
+	border-radius: 7px;
+	border: solid 1px var(--color-accent);
+	padding: 1rem 0.5rem;
+	cursor: pointer;
+}
+.link-button:hover {
+	background: var(--color-accent);
+	color: var(--color-background);
+}
+.link-button:hover, .link-button:hover a {
+	color: var(--color-background);
+}
+</style>
---
+++ src/routes/work/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import ProjectList from "$components/ProjectList/ProjectList.svelte";
+	import projects from "$data/projects.yml";
+</script>
+<main class="main-width">
+	<h1>Selected work</h1>
+	<ProjectList {projects} show_images />
+</main>
